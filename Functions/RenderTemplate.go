6b06689cef1d7065bff2dea14b256415fbe91aa5
ismailsayen <ismailsvn02@gmail.com>
2024-09-30 16:08:06 +0100

    fianl version

--- Functions/RenderTemplate.go
+++ Functions/RenderTemplate.go
@@ -6,7 +6,8 @@ import (
 	"text/template"
 )
 
-func RenderTemplate(w http.ResponseWriter, tmpl string, data string) error {
+func RenderTemplate(w http.ResponseWriter, status int, tmpl string, data string) error {
+	w.WriteHeader(status)
 	t, err := template.ParseFiles(tmpl)
 	if err != nil {
 		fmt.Println("Error parsing template:", err)
--- main.go
+++ main.go
@@ -10,39 +10,37 @@ import (
 func Handler(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
-		err := asciiartweb.RenderTemplate(w, "./templates/index.html", "")
+		w.WriteHeader(http.StatusOK)
+		err := asciiartweb.RenderTemplate(w, http.StatusOK, "./templates/index.html", "")
 		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			asciiartweb.RenderTemplate(w, "./templates/500Page.html", "/")
+			http.Error(w, "We're sorry, but something went wrong on our end. Please try again later.", http.StatusInternalServerError)
 			return
 		}
-		w.WriteHeader(http.StatusOK)
 	case "/ascii-art":
 		if r.Method != "POST" {
+			err := asciiartweb.RenderTemplate(w, http.StatusBadRequest, "./templates/400Page.html", "/")
+			if err != nil {
+				http.Error(w, "We're sorry, but something went wrong on our end. Please try again later.", http.StatusInternalServerError)
+				return
+			}
 		} else if r.Method == "POST" {
 			r.ParseForm()
 			result := asciiartweb.Ascii(r.FormValue("text"), r.FormValue("banner"))
-			err := asciiartweb.RenderTemplate(w, "./templates/index.html", result)
+			err := asciiartweb.RenderTemplate(w, http.StatusOK, "./templates/index.html", result)
 			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				asciiartweb.RenderTemplate(w, "./templates/500Page.html", "/")
-				return
 			}
-			w.WriteHeader(http.StatusOK)
 		}
 	default:
-		err := asciiartweb.RenderTemplate(w, "./templates/404Page.html", "/")
+		err := asciiartweb.RenderTemplate(w, http.StatusNotFound, "./templates/404Page.html", "/")
 		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			asciiartweb.RenderTemplate(w, "./templates/500Page.html", "/")
+			http.Error(w, "We're sorry, but something went wrong on our end. Please try again later.", http.StatusInternalServerError)
 			return
 		}
-		w.WriteHeader(http.StatusNotFound)
 
 	}
 }
 
 func main() {
 	http.HandleFunc("/", Handler)
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
